c45af61de172c467be2fb7be0d26b36ec3d0e1ec
Maxwell Tippetts <104464940+MrTurtleman1@users.noreply.github.com>
2023-11-10 16:49:34 -0700

    w02 task attempted

--- w02-task/scripts/w02-task.js
+++ w02-task/scripts/w02-task.js
@@ -3,27 +3,35 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
+let fullName = 'Max Tippetts';
+let curentYear = new Date().getFullYear();
+let profilePicture = 'images/me.jpeg';
 
 
 
 /* Step 3 - Element Variables */
-
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.getElementById('year');
+const imageElement = profilePicture;
 /* Step 4 - Adding Content */
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = `${curentYear}`;
+imageElement.setAttribute = ('src', profilePicture);
+imageElement.setAttribute = ('alt', `Profile image of ${fullName}`);
 
 
 
 /* Step 5 - Array */
-
-
-
+let starches = ['pasta', ' potatoes', ' bread'];
+foodElement.innerHTML += `<br>${starches}`;
+let fruit = ' bananas';
+starches.push(fruit);
+foodElement.innerHTML += `<br>${starches}`;
+starches.shift();
+foodElement.innerHTML += `<br>${starches}`;
+starches.pop()
+foodElement.innerHTML += `<br>${starches}`;
 
 
 
